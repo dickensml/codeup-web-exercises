6c3d2bc309ebc168b8cfb47f21ab8efba5171d34
Michael Dickens <dickens.michael64@gmail.com>
2022-04-17 18:36:17 -0500

    sending up redo JS with HTML of exercises

--- js/external.js
+++ js/external.js
@@ -1,18 +1,35 @@
 
 
     // console.log('Hello from external Javascript');
-    //
     // alert('Welcome to my website!')
-    //
     // var color = prompt('What is your favorite color?');
-    //
     // alert('Great! ' + color +  ' is mhy favorite color, too.');
 
 
-    var littleMerm = (3);
-    var broBear = (5);
-    var Hercules = (1);
-    var rentalPrice = (3);
-    var totalpaid = (littleMerm + broBear + Hercules) * rentalPrice;
-    console.log (totalpaid);
+    // var littleMerm = (3);
+    // var broBear = (5);
+    // var Hercules = (1);
+    // var rentalPrice = (3);
+    // var totalpaid = (littleMerm + broBear + Hercules) * rentalPrice;
+    // alert(totalpaid);
 
+    // var googHours = (6);
+    // var amazHours = (4);
+    // var FBhours = (10);
+    // var Grate = (400);
+    // var Arate = (380);
+    // var FBrate = (350);
+    // var totPay = ((googHours * Grate) + (amazHours * Arate) + (FBhours * FBrate));
+    // alert(totPay);
+
+    // var classNotFull = (true);
+    // var sched = (true);
+    // var canEnroll = (classNotFull && sched);
+    // alert(canEnroll);
+
+    var items = (false);
+    var offerGood = (false);
+    var premMem = (true);
+
+    var applyOffer = ((items && offerGood) || premMem);
+    alert(applyOffer);
