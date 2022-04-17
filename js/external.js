752494a31f97b29dadd0eed0033926855eb05cb3
Michael Dickens <dickens.michael64@gmail.com>
2022-04-17 17:02:29 -0500

    redoing exercises

---
+++ js/external.js
@@ -0,0 +1,18 @@
+
+
+    // console.log('Hello from external Javascript');
+    //
+    // alert('Welcome to my website!')
+    //
+    // var color = prompt('What is your favorite color?');
+    //
+    // alert('Great! ' + color +  ' is mhy favorite color, too.');
+
+
+    var littleMerm = (3);
+    var broBear = (5);
+    var Hercules = (1);
+    var rentalPrice = (3);
+    var totalpaid = (littleMerm + broBear + Hercules) * rentalPrice;
+    console.log (totalpaid);
+
