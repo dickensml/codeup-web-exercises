89e22249d00eefc9e74d6105370c9971b15ae4d7
Michael Dickens <dickens.michael64@gmail.com>
2022-05-09 09:32:37 -0500

    js warmup

---
+++ js-warm-ups/warmup-array-average.js
@@ -0,0 +1,12 @@
+
+// var array = [3, 5, 7, 10, 0];
+// var array2 = [0, 3, 5, 6, 21]
+
+function average(array) {
+    var total = 0;
+    for (var i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    return total /array.length;
+}
+console.log (average(array[3, 5, 7, 10, 0]));
