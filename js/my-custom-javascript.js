b4b2d40c7ddd7f071f336798245834d70d7fcdd0
Michael Dickens <dickens.michael64@gmail.com>
2022-05-13 11:28:26 -0500

    more jQuery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,7 +1,34 @@
 "use strict";
-
+"use strict";
 $(document).ready(function() {
+   // jQuery - Select element with ID of 'pageLink' and set innerHTML using html() method
 
-    // INSERT JAVASCRIPT CODE SAMPLE HERE
+   // ID Selector
+   $('#pageLink').html("<h1>Link to Somewhere</h1>");
+
+   // Vanilla JS way
+   // document.getElementById("pageLink").innerHTML = "<h1>Link to Somewhere</h1>";
+
+   // Class Selector
+   // returns an array of elements
+   console.log($('.list-item'));
+   // $('.list-item')[0].innerHTML = "hello";
+
+
+   // Element Selector
+
+   // If you want to change more than one property, you can pass an object with key-value pairs (property, value)
+
+   // $('li').css({
+   //     'background-color': 'hotpink',
+   //     'border-bottom': '1px solid black',
+   // })
+   $('li').css('background-color', 'rebeccapurple');
+
+   // Selecting Multiple Elements -> returns array of element nodes
+   $('h1, #pageLink, #list').css('color', 'forestgreen')
+
+   // All Selector -> select every element on the page
+   $('*').css('border', '2px dotted black')
 
 });
