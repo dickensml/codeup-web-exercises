bb96e7ea38641681e1981efcaa9cdfb4e901b44d
Michael Dickens <dickens.michael64@gmail.com>
2022-05-16 22:07:25 -0500

    pushing up updates for some exercises that were incomplete

---
+++ js/break_and_continue.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var number = prompt('Please enter an odd number between 1 and 50: ');
+
+if (number % 2 === 0) {
+    alert('That is an even number. Try, again.')
+}else {
+        alert('Well done!')
+    }
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,28 +1,54 @@
-//var i = 0;
+
+'use strict';
+
+
+// 2. Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+// var input = 5;
 //
-//             do {
-//                 console.log('while loop iteration # is ' + i);
-//                 i += 2;
-//             } while (i <= 10);
+// function showMultiplicationTable(input) {
+//     let count = 1;
+//     let amount = 'number';
+//     for (let i = 0; i <= 10; i++) {
+//         amount = input * count;
+//     }
+//     return(input + ' x ' + count + ' = ' + amount);
+// }
+// console.log((showMultiplicationTable(input)));
+
+
+// var min = 20;
+// var max = 200;
+// function getRandomArbitrary(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+
+// 3. Use a forUse a for  loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
+
+//     function randomOddandEven(input) {
+//     let num = Math.random() * (200 - 20) + 20;
+//     for (let i = 0; i < 10; i++) {
 //
-//how many inputs?   1
-// what type of inputs?  number
-// what type if outputs?  number
+//     if (num[i] % 2 === 0) {
+//         console.log(num + ' is even')
+//     } else {
+//             console.log (num + ' is odd')
+//         }
+//     }
+// }
+// randomOddandEven(input);
 
-    //var inPutNum = 'number';
+
+// 4. Create a for loop that uses console.log to create the output shown below.
 
 
+// 5. Create a for loop that uses console.log to create the output shown below.
 
-    // function showMultiplicationTable(input) {
-    //     var inPut =
-    //     var multNum = 0;
-    //
-    // var input = parseInt(prompt ('Input a number: '));
-    //
-    //     multNum ++;
-    //     while (multNum <= 10) {
-    //
-    //       console.log(input * multNum);
-    //   }
-    //
-    // }
+// function countDownByFive() {
+//     let num = 100;
+//     for (let i = 0; i < 20; i++) {
+//         num = num - 5;
+//     }
+// }
+// console.log(countDownByFive());
