74a7b6ba574f346ddf3ae8ac6bd30c7bcb19a072
Michael Dickens <dickens.michael64@gmail.com>
2022-05-17 09:08:57 -0500

    added ScratchPad files for practice

---
+++ js/ScratchPad.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var customerObj = {
+    customerName: "Barbara",
+    items: [
+        {name: 'tomatoes', price: 4},
+        {name: 'potatoes', price: 6},
+        {name: 'wine', price: 16},
+    ],
+    clubMember: false
+}
+
+var customerArrOfObj = [
+    {
+        customerName: "Barbara",
+        items: [
+            {name: 'tomatoes', price: 4},
+            {name: 'potatoes', price: 6},
+            {name: 'wine', price: 16},
+        ],
+        clubMember: false
+    },
+    {
+        customerName: "Dale",
+        items: [
+            {name: 'television', price: 400},
+            {name: 'xbox', price: 599},
+            {name: 'soda', price: 1},
+        ],
+        clubMember: true
+    },
+    {
+        customerName: "Kenneth",
+        items: [
+            {name: 'doughnuts', price: 7},
+            {name: 'apple juice', price: 3},
+            {name: 'bird seed', price: 13},
+        ],
+        clubMember: true
+    }]
