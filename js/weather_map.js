ba24e93676a36e33af60a0c693eb5419b9605c5a
Michael Dickens <dickens.michael64@gmail.com>
2022-05-22 22:59:36 -0500

    added the mapbox geocoder js

--- js/weather_map.js
+++ js/weather_map.js
@@ -37,8 +37,6 @@
 
 //returns current weather in Farhenheit, by coords:
 
-
-
 $.get("https://api.openweathermap.org/data/2.5/weather", {
     APPID: OPEN_WEATHER_APPID,
     lat:    30.50919521933195,
@@ -61,3 +59,15 @@ $.get("https://api.openweathermap.org/data/2.5/forecast", {
 }).done(function(data) {
     console.log('5 day forecast', data);
 });
+
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
