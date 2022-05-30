f291b9763e35d44835f7237f6bb2e18da3e0da90
Michael Dickens <dickens.michael64@gmail.com>
2022-05-30 14:47:33 -0500

    pushing up files and stuff from promise lecture/exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,8 +38,43 @@ const users = [
     }
 ];
 
+
+// 2.  Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+
 // let under50 = products.filter(products => products.price < 50);
 
-let langArr = users.filter(users => users.languages > 2);
+let langArr = users.filter(user => user.languages.length >= 3);
 
-console.log(langArr);
+console.log("Users lang: ",langArr);
+
+// 3. Use .map to create an array of strings where each element is a user's email address
+
+let userEmailArr =  users.map(user => user.email);
+console.log("emeils: ", userEmailArr);
+
+//  4.  Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average
+
+const yearsExp = users.reduce((bucket, currentValue) => {
+    console.log("bucket, currentValue: ", bucket, currentValue) ;
+    return bucket + currentValue.yearsOfExperience
+}, 0);
+console.log("Total Years of Experience: ", yearsExp);
+
+const avgYears = yearsExp / users.length;
+console.log("Average Years: ", avgYears)
+
+//  5.  Use .reduce to get the longest email from the list of users.
+
+
+
+
+
+// 6.  Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+const usersNames = users.reduce((bucket, currentValue))
+
+
+// Bonus:  Use .reduce to get the unique list of languages from the list of users.
+
+
+
---
+++ js/promises.js
@@ -0,0 +1,6 @@
+"use script";
+
+// https://api.github.com/users/dickensml/events/public
+
+fetch = ("https://api.github.com/users/dickensml/events/public", {headers: {'Authorization': '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'}})
+
